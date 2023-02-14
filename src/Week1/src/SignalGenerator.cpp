8b2a30e3ca501d6f81abbf865be1b79b517d1e0e
Pablo Ortega-Kral <2001.paok@gmail.com>
2023-02-14 03:23:54 +0000

    Started publisher node

--- src/Week1/src/SignalGenerator.cpp
+++ src/Week1/src/SignalGenerator.cpp
@@ -1,5 +1,15 @@
 #include <ros/ros.h>
 
 int main(int argc, char* argv[]){
-  ros::init(argc, argv, "vision_node");
+  ros::init(argc, argv, "signal_processor");
+  ros::NodeHandle handler;
+  ros::Publisher publisher = handler.advertise<std_msgs::Int32>("/signal", 10);
+  ros::Rate rate(10); // ROS Rate at 10Hz
+
+  while (ros::ok()) {
+        std_msgs::Int32 signal;
+        signal.data = 3;
+        publisher.publish(signal);
+        rate.sleep();
+    }
 }
--- src/Week1/src/SignalProcessor.cpp
+++ src/Week1/src/SignalProcessor.cpp
@@ -1,5 +1,7 @@
 #include <ros/ros.h>
 
 int main(int argc, char* argv[]){
-  ros::init(argc, argv, "vision_node");
+  ros::init(argc, argv, "signal_processor");
+  ros::NodeHandle handler;
+  ros::Subscriber suscriber;
 }
