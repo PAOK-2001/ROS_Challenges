dc1e32119e8d45c88db196c3884f0993dc8b3787
Pablo Ortega <2001@paok.com>
2023-02-16 01:13:13 +0000

    Added time and procSignal topics. Fixed launch file

--- src/week1_signal_nodes/src/process.py
+++ src/week1_signal_nodes/src/process.py
@@ -1,14 +1,22 @@
 #! /usr/bin/env python
 import rospy
+import threading as thrd
 from std_msgs.msg import Float32
 
-
+signal_data = 0
+    
 def signalCallback(signal):
+    global signal_data
+    signal_data = signal.data
     rospy.loginfo(signal)
-
 
 if __name__ == '__main__':
     rospy.init_node("signal_processor")
-    sub = rospy.Subscriber("/signal", Float32 ,signalCallback)
-    rospy.Rate(20)
-    rospy.spin()
+    pub = rospy.Publisher("/procSignal", Float32, queue_size=20)
+    signalSub = rospy.Subscriber("/signal", Float32 ,signalCallback)
+    time = rospy.Subscriber("/time", Float32)
+    rate = rospy.Rate(20)
+    while not rospy.is_shutdown():
+        proc = (signal_data+2.224) *1.1518
+        pub.publish(proc)
+        rate.sleep()
--- src/week1_signal_nodes/src/signal_generator.cpp
+++ src/week1_signal_nodes/src/signal_generator.cpp
@@ -5,17 +5,20 @@
 
 int main(int argc, char* argv[]){
   ros::init(argc, argv, "signal_generator");
-  ros::NodeHandle handler;
-  ros::Publisher publisher = handler.advertise<std_msgs::Float32>("/signal", 10);
-  ros::Rate rate(20); // ROS Rate at 10Hz
+  ros::NodeHandle handler1 , handler2;
+  ros::Publisher signalPub = handler1.advertise<std_msgs::Float32>("/signal", 20);
+  ros::Publisher timePub = handler2.advertise<std_msgs::Float32>("/time", 20);
+  ros::Rate rate(20); // ROS Rate at 20Hz
 
-  float x = 0;
+  std_msgs::Float32  x;
+  x.data = 0;
   std_msgs::Float32 signal;
   while (ros::ok()) {
-        signal.data = tan(pow(log(1/exp(sin(x))),3))+cos(x);
+        signal.data = tan(pow(log(1/exp(sin(x.data))),3))+cos(x.data);
         ROS_INFO_STREAM("Sending " << signal.data);
-        publisher.publish(signal);
-        x+=0.1;
+        x.data+=0.05;
+        signalPub.publish(signal);
+        timePub.publish(x);
         rate.sleep();
     }
 }
