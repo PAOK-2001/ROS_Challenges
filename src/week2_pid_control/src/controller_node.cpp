bd0c2edacf8818e6cb4e53bf3ac21bb2813c0247
PAOK-2001 <ubuntu@ros1-melodic.lxd>
2023-02-22 08:06:00 +0000

    First attempt

--- src/week2_pid_control/src/controller_node.cpp
+++ src/week2_pid_control/src/controller_node.cpp
@@ -1,7 +1,58 @@
 #include <ros/ros.h>
 #include <ros/console.h>
+#include <std_msgs/Float32.h>
+#include <std_msgs/String.h>
 #include <cmath>
+#include <string>
+#include <iostream>
+#include "week2_pid_control/motor_input.h"
+#include "week2_pid_control/motor_output.h" 
+#include "week2_pid_control/set_point.h"
+
+float motor_out;
+
+void receive_feedback(const week2_pid_control::motor_output::ConstPtr &msg) {
+  std::cout << "Si caben o no?" << std::endl;
+   motor_out = msg->output;
+}
 
 int main(int argc, char* argv[]){
   ros::init(argc, argv, "controller_node");
+  ros::NodeHandle handler;
+  ros::NodeHandle handler2;
+  int nodeRate = 20;
+  float Kp, Ti, Td;
+  ros::param::get("/system_node_rate", nodeRate);
+  ros::param::get("/Kp",Kp);
+  ros::param::get("/Ti",Ti);
+  ros::param::get("/Td",Td);
+  
+  ros::Subscriber systemFeedback = handler.subscribe("/motor_output", 10, receive_feedback);
+  ros::Publisher controllerOutput = handler2.advertise<week2_pid_control::motor_input>("/motor_input", 10);
+  ros::Rate rate(nodeRate); 
+  double time = ros::Time::now().toSec();
+  float error, rateError, dTime;
+  float lastError = 0.0;
+  float cumError = 0.0;
+  float ref;
+  week2_pid_control::motor_input motor_in;
+  motor_in.input = 0.0;
+  controllerOutput.publish(motor_in);
+  while (ros::ok()) {
+        ros::param::get("/set_point",ref);
+        dTime = ros::Time::now().toSec()- time;
+        time = ros::Time::now().toSec();
+
+        if(dTime > 0) {
+          error = ref - motor_out;
+          cumError += error*dTime;
+          rateError = (error - lastError) / dTime;
+          lastError = error;
+          motor_in.input = Kp*error;
+          controllerOutput.publish(motor_in);
+          std::cout << motor_out << "\n";
+        }
+
+        rate.sleep();
+    }
 }
