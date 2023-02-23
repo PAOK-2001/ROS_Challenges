92688d3ccfa8411a6ec6f1f15eb24cf57bf9acf7
PacoRmzA <a01634262@itesm.mx>
2023-02-22 20:11:08 -0600

    Week 2 activities

--- src/week1_signal_nodes/src/process.py
+++ src/week1_signal_nodes/src/process.py
@@ -2,19 +2,19 @@
 import rospy
 import threading as thrd
 from std_msgs.msg import Float32
+from week1_signal_nodes.msg import signal_msg  #Import the message to be used
 
 signal_data = 0
     
 def signalCallback(signal):
     global signal_data
-    signal_data = signal.data
+    signal_data = signal.signal_y
     rospy.loginfo(signal)
 
 if __name__ == '__main__':
     rospy.init_node("signal_processor")
     pub = rospy.Publisher("/procSignal", Float32, queue_size=20)
-    signalSub = rospy.Subscriber("/signal", Float32 ,signalCallback)
-    time = rospy.Subscriber("/time", Float32)
+    signalSub = rospy.Subscriber("/signal", signal_msg ,signalCallback)
     rate = rospy.Rate(20)
     while not rospy.is_shutdown():
         proc = (signal_data+2.224) *1.1518
--- src/week1_signal_nodes/src/signal_generator.cpp
+++ src/week1_signal_nodes/src/signal_generator.cpp
@@ -2,23 +2,22 @@
 #include <std_msgs/Float32.h>
 #include <cmath>
 #include <ros/console.h>
+#include "week1_signal_nodes/signal_msg.h"
+
 
 int main(int argc, char* argv[]){
   ros::init(argc, argv, "signal_generator");
   ros::NodeHandle handler1 , handler2;
-  ros::Publisher signalPub = handler1.advertise<std_msgs::Float32>("/signal", 20);
-  ros::Publisher timePub = handler2.advertise<std_msgs::Float32>("/time", 20);
+  ros::Publisher signalPub = handler1.advertise<week1_signal_nodes::signal_msg>("/signal", 20);
   ros::Rate rate(20); // ROS Rate at 20Hz
 
-  std_msgs::Float32  x;
-  x.data = 0;
-  std_msgs::Float32 signal;
+  week1_signal_nodes::signal_msg output;
+  output.time_x = 0;
   while (ros::ok()) {
-        signal.data = tan(pow(log(1/exp(sin(x.data))),3))+cos(x.data);
-        ROS_INFO_STREAM("Sending " << signal.data);
-        x.data+=0.05;
-        signalPub.publish(signal);
-        timePub.publish(x);
+        output.signal_y = tan(pow(log(1/exp(sin(output.time_x))),3))+cos(output.time_x);
+        ROS_INFO_STREAM("Sending " << output.signal_y);
+        output.time_x+=0.05;
+        signalPub.publish(output);
         rate.sleep();
     }
 }
