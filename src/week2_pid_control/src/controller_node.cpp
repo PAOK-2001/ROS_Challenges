376b366b00015db4f41e58a97ce5e57dc59c5ddf
ExquisiterEmil <emil.dobetsberger@gmail.com>
2023-02-23 12:48:42 -0600

    changed setpoint to be a topic and fixed uninitialized conditions

--- src/week2_pid_control/src/controller_node.cpp
+++ src/week2_pid_control/src/controller_node.cpp
@@ -8,45 +8,57 @@
 #include "week2_pid_control/motor_input.h"
 #include "week2_pid_control/motor_output.h" 
 
-float motor_out;
+float motor_out = 0, ref = 0;
+bool motor_init = false;
 
 void receive_feedback(const week2_pid_control::motor_output::ConstPtr &msg) {
   motor_out = msg->output;
+  motor_init = true;
+}
+
+void receive_setpoint(const std_msgs::Float32::ConstPtr &setpoint) {
+  ref = setpoint->data;
 }
 
 int main(int argc, char* argv[]){
   ros::init(argc, argv, "controller_node");
   ros::NodeHandle handler;
   ros::NodeHandle handler2;
-  int nodeRate = 20;
+  int nodeRate = 1000;
   float Kp, Ti, Td;
   ros::param::get("/system_node_rate", nodeRate);
   ros::param::get("/Kp",Kp);
   ros::param::get("/Ti",Ti);
   ros::param::get("/Td",Td);
+  ros::param::get("/set_point",ref);
   
   ros::Subscriber systemFeedback = handler.subscribe("/motor_output", 10, receive_feedback);
   ros::Publisher controllerOutput = handler2.advertise<week2_pid_control::motor_input>("/motor_input", 10);
+  ros::Subscriber setPointSubscr = handler.subscribe("/setpoint", 10, receive_setpoint);
   ros::Rate rate(nodeRate); 
   double time = ros::Time::now().toSec();
   float error, rateError, dTime;
   float lastError = 0.0;
   float cumError = 0.0;
-  float ref;
   week2_pid_control::motor_input motor_in;
   motor_in.input = 0.0;
   controllerOutput.publish(motor_in);
   while (ros::ok()) {
-        ros::param::get("/set_point",ref);
-        dTime = ros::Time::now().toSec()- time;
-        time = ros::Time::now().toSec();
         
-        if(dTime > 0) {
+        dTime = ros::Time::now().toSec()- time;
+        std::cout << dTime << "\n";
+        const float minDeltaTime = 0.0001;
+        if(dTime > minDeltaTime && motor_init) {
+          time = ros::Time::now().toSec();
           error = ref - motor_out;
           cumError += error*dTime;
           rateError = (error - lastError) / dTime;
           lastError = error;
-          motor_in.input = (Kp*error)+(Td*rateError)+(Ti*cumError);
+          motor_in.input = (Kp*error)+(Td*minDeltaTime*rateError)+(Ti*cumError);
+          controllerOutput.publish(motor_in);
+        } else {
+          week2_pid_control::motor_input motor_in;
+          motor_in.input = 0;
           controllerOutput.publish(motor_in);
         }
         ros::spinOnce();
--- src/week2_pid_control/src/setpoint_generator.cpp
+++ src/week2_pid_control/src/setpoint_generator.cpp
@@ -6,11 +6,20 @@
 int main(int argc, char* argv[]){
   ros::init(argc, argv, "setpoint_generator");
   ros::NodeHandle handler;
+  ros::Publisher setpointPublisher = handler.advertise<std_msgs::Float32>("/setpoint", 10);
   double time = ros::Time::now().toSec();
+  
+  int nodeRate = 1000;
+  ros::param::get("/system_node_rate", nodeRate);
+  ros::Rate rate(nodeRate); 
+
   while (ros::ok()) {
     time = ros::Time::now().toSec();
-    double ref = abs(13*tan(pow(log(1/exp(sin(time))),3))+cos(time));
-    ros::param::set("/set_point",ref);
+    float refVal = abs(3*tan(pow(log(1/exp(sin(time*0.3))),3))+cos(time*0.3));
+    std_msgs::Float32 ref;
+    ref.data = refVal;
+    setpointPublisher.publish(ref);
+    rate.sleep();
   }
 
 }
